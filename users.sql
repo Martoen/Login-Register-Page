bc5f639738148ad058a1e8a7c3fdd2b55e89c278
Martoen <martijnschermers2@gmail.com>
2021-10-16 10:50:50 +0200

    SQL syntax fix

--- users.sql
+++ users.sql
@@ -1,9 +1,9 @@
 -- Import this file into your database
 
-create table users (
-    user_id int(11) not null PRIMARY KEY AUTO_INCREMENT,
-    user_name varchar(128) not null, 
-    user_mail varchar(255) not null, 
-    user_uid varchar(128) not null, 
-    user_pwd varchar(128) not null
+CREATE TABLE users (
+    user_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_name varchar(128) NOT NULL, 
+    user_mail varchar(255) NOT NULL, 
+    user_uid varchar(128) NOT NULL, 
+    user_pwd varchar(128) NOT NULL
 );
